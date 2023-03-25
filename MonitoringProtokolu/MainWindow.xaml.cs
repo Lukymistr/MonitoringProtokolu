cc90e80963b80d3e4a713741e391187830dcf859
Lukymistr <Lukymistr1@seznam.cz>
2023-03-25 19:46:39 +0100

    oprava drobností
    aplikace se nyní může roztahovat / zmenšovat
    aplikace nyní může jít do full screenu a zpět

--- MonitoringProtokolu/MainWindow.xaml.cs
+++ MonitoringProtokolu/MainWindow.xaml.cs
@@ -1,23 +1,55 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Shell;
 
 namespace MonitoringProtokolu {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            WindowChrome.SetWindowChrome(this, new WindowChrome {CaptionHeight = 0});
+            
+            gridFile.Visibility = Visibility.Visible;
+
         }
+        private void Window_StateChanged(object sender, EventArgs e) {
+            if (WindowState == WindowState.Maximized) {
+                WindowState = WindowState.Normal;
+                Left = 0;
+                Top = 0;
+                Width = SystemParameters.PrimaryScreenWidth;
+                Height = SystemParameters.PrimaryScreenHeight;
+                WindowChrome.SetWindowChrome(this, null);
+            }
+        }
+
         private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e) {
             if (e.ChangedButton == MouseButton.Left) {
                 if (e.ClickCount == 2) {
-                    if (WindowState == WindowState.Normal) {
-                        WindowState = WindowState.Maximized;
-                    } else {
-                        WindowState = WindowState.Normal;
-                    }
+                    changeSize();
                 } else {
-                    DragMove();
+                    if (!(Width == SystemParameters.PrimaryScreenWidth && Height == SystemParameters.PrimaryScreenHeight)) {
+                        DragMove();
+                    }
+                        
                 }
+            }
+        }
+
+        private void changeSize() {
+            if (Width == SystemParameters.PrimaryScreenWidth && Height == SystemParameters.PrimaryScreenHeight) {
+                Width = MinWidth;
+                Height = MinHeight;
+                Left = (SystemParameters.PrimaryScreenWidth - Width) / 2;
+                Top = (SystemParameters.PrimaryScreenHeight - Height) / 2;
+                WindowChrome.SetWindowChrome(this, new WindowChrome { CaptionHeight = 0 });
+            } else {
+                WindowChrome.SetWindowChrome(this, null);
+                WindowState = WindowState.Normal;
+                Left = 0;
+                Top = 0;
+                Width = SystemParameters.PrimaryScreenWidth;
+                Height = SystemParameters.PrimaryScreenHeight;
             }
         }
 
@@ -26,14 +58,51 @@ namespace MonitoringProtokolu {
         }
 
         private void btnTitleBarResize_Click(object sender, RoutedEventArgs e) {
-            if (WindowState == WindowState.Maximized) {
-                WindowState = WindowState.Normal;
-            } else {
-                WindowState = WindowState.Maximized;
-            }
+            changeSize();
         }
         private void btnTitleBarExit_Click(object sender, RoutedEventArgs e) {
-            Environment.Exit(0);
+            Close();
+        }
+
+        private void btnFiles_Click(object sender, RoutedEventArgs e) {
+            gridFile.Visibility = Visibility.Visible;
+            gridDirectory.Visibility = Visibility.Hidden;
+            gridGlobalSettings.Visibility = Visibility.Hidden;
+            gridSMTP.Visibility = Visibility.Hidden;
+        }
+
+        private void btnDirectory_Click(object sender, RoutedEventArgs e) {
+            gridFile.Visibility = Visibility.Hidden;
+            gridDirectory.Visibility = Visibility.Visible;
+            gridGlobalSettings.Visibility = Visibility.Hidden;
+            gridSMTP.Visibility = Visibility.Hidden;
+        }
+
+        private void btnGlobalSettings_Click(object sender, RoutedEventArgs e) {
+            gridFile.Visibility = Visibility.Hidden;
+            gridDirectory.Visibility = Visibility.Hidden;
+            gridGlobalSettings.Visibility = Visibility.Visible;
+            gridSMTP.Visibility = Visibility.Hidden;
+        }
+
+        private void btnSmtpSettings_Click(object sender, RoutedEventArgs e) {
+            gridFile.Visibility = Visibility.Hidden;
+            gridDirectory.Visibility = Visibility.Hidden;
+            gridGlobalSettings.Visibility = Visibility.Hidden;
+            gridSMTP.Visibility = Visibility.Visible;
+        }
+
+        private void btnTurnOnOff_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e) {
+            Hide();
+            // Show(); inverze
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e) {
+            Close();
         }
     }
 }
